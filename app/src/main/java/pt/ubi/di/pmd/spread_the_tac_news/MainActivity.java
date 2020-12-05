1d47060db4a7234ff16ff6a1c99cf9132e0182c8
pipamata <pipamata@gmail.com>
2020-12-05 16:20:43 +0000

    Initial commit

---
+++ app/src/main/java/pt/ubi/di/pmd/spread_the_tac_news/MainActivity.java
@@ -0,0 +1,14 @@
+package pt.ubi.di.pmd.spread_the_tac_news;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
