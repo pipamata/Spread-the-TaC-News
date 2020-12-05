e2b060568fa44c53ff89c8c3ab33d0ee313072d8
DiasSilva <renato_driver20@sapo.pt>
2020-12-05 18:46:57 +0000

    new branch teste

--- app/src/main/java/pt/ubi/di/pmd/spread_the_tac_news/MainActivity.java
+++ app/src/main/java/pt/ubi/di/pmd/spread_the_tac_news/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         // new branch
+        // será que é desta?
         Toast.makeText(this, "TEste!!", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "O meu teste é mais fofo que o teu :b!!", Toast.LENGTH_SHORT).show();
     }
