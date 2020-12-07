c178a5360ad548407fd4b5ed7f8102728580d4a2
DiasSilva <renato_driver20@sapo.pt>
2020-12-07 18:59:12 +0000

    Adicionei uma activity onde vamos ver as noticias, Adicionei 2 layouts 1 onde as noticias aparecem outro que cria o objecto das noticias e os importa pelo codigo para o feed de noticas. Desta maneira permite-nos aceder mais facilmente a cada item que adicionamos e usar uma base de dados para modificar as noticias.

--- app/src/main/java/pt/ubi/di/pmd/spread_the_tac_news/MainActivity.java
+++ app/src/main/java/pt/ubi/di/pmd/spread_the_tac_news/MainActivity.java
@@ -33,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                getWebsite();
+               // getWebsite();
             }
         });
     }
